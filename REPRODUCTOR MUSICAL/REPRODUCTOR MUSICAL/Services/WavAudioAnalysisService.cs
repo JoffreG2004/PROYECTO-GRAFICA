36604d7140b83d3d30da98e56cfda3378946d10c
Joffre Esteban Gomez Quinaluisa <joffre_gomez@hotmail.com>
2026-06-15 22:45:28 -0500

    proyecto 4.0

--- REPRODUCTOR MUSICAL/REPRODUCTOR MUSICAL/Services/WavAudioAnalysisService.cs
+++ REPRODUCTOR MUSICAL/REPRODUCTOR MUSICAL/Services/WavAudioAnalysisService.cs
@@ -45,60 +45,121 @@ namespace REPRODUCTOR_MUSICAL.Services
 
         private void TryLoadPcmWav(string filePath)
         {
-            using (var reader = new BinaryReader(File.OpenRead(filePath)))
+            try
             {
-                if (new string(reader.ReadChars(4)) != "RIFF")
+                using (var reader = new BinaryReader(File.OpenRead(filePath)))
                 {
-                    return;
-                }
-
-                reader.ReadInt32();
-
-                if (new string(reader.ReadChars(4)) != "WAVE")
-                {
-                    return;
-                }
-
-                var bitsPerSample = 0;
-                byte[] dataBytes = null;
-
-                while (reader.BaseStream.Position < reader.BaseStream.Length)
-                {
-                    var chunkId = new string(reader.ReadChars(4));
-                    var chunkSize = reader.ReadInt32();
-                    var nextChunk = reader.BaseStream.Position + chunkSize;
-
-                    if (chunkId == "fmt ")
+                    if (ReadFourCc(reader) != "RIFF")
                     {
-                        var audioFormat = reader.ReadInt16();
-                        channels = reader.ReadInt16();
-                        sampleRate = reader.ReadInt32();
-                        reader.ReadInt32();
-                        reader.ReadInt16();
-                        bitsPerSample = reader.ReadInt16();
+                        return;
+                    }
 
-                        if (audioFormat != 1 || bitsPerSample != 16)
+                    reader.ReadUInt32();
+
+                    if (ReadFourCc(reader) != "WAVE")
+                    {
+                        return;
+                    }
+
+                    var bitsPerSample = 0;
+                    byte[] dataBytes = null;
+
+                    while (TryReadChunkHeader(reader, out var chunkId, out var chunkSize))
+                    {
+                        var chunkStart = reader.BaseStream.Position;
+                        var bytesRemaining = reader.BaseStream.Length - chunkStart;
+                        var actualChunkSize = GetReadableChunkSize(chunkSize, bytesRemaining);
+                        var nextChunk = chunkStart + actualChunkSize + (actualChunkSize % 2);
+
+                        if (actualChunkSize < 0)
                         {
                             return;
                         }
+
+                        if (chunkId == "fmt ")
+                        {
+                            if (actualChunkSize < 16)
+                            {
+                                return;
+                            }
+
+                            var audioFormat = reader.ReadInt16();
+                            channels = reader.ReadInt16();
+                            sampleRate = reader.ReadInt32();
+                            reader.ReadInt32();
+                            reader.ReadInt16();
+                            bitsPerSample = reader.ReadInt16();
+
+                            if (audioFormat != 1 || bitsPerSample != 16 || channels <= 0 || sampleRate <= 0)
+                            {
+                                return;
+                            }
+                        }
+                        else if (chunkId == "data")
+                        {
+                            if (actualChunkSize > int.MaxValue)
+                            {
+                                return;
+                            }
+
+                            var bytesToRead = (int)(actualChunkSize - (actualChunkSize % 2));
+                            dataBytes = reader.ReadBytes(bytesToRead);
+                        }
+
+                        reader.BaseStream.Position = Math.Min(nextChunk, reader.BaseStream.Length);
                     }
-                    else if (chunkId == "data")
+
+                    if (dataBytes == null || dataBytes.Length == 0 || bitsPerSample != 16)
                     {
-                        dataBytes = reader.ReadBytes(chunkSize);
+                        return;
                     }
 
-                    reader.BaseStream.Position = Math.Min(nextChunk, reader.BaseStream.Length);
+                    samples = new short[dataBytes.Length / 2];
+                    Buffer.BlockCopy(dataBytes, 0, samples, 0, samples.Length * 2);
+                    hasRealSamples = samples.Length > 0;
                 }
-
-                if (dataBytes == null || dataBytes.Length == 0 || bitsPerSample != 16)
-                {
-                    return;
-                }
-
-                samples = new short[dataBytes.Length / 2];
-                Buffer.BlockCopy(dataBytes, 0, samples, 0, dataBytes.Length);
-                hasRealSamples = samples.Length > 0;
             }
+            catch (IOException)
+            {
+            }
+            catch (ArgumentException)
+            {
+            }
+        }
+
+        private static string ReadFourCc(BinaryReader reader)
+        {
+            return new string(reader.ReadChars(4));
+        }
+
+        private static bool TryReadChunkHeader(BinaryReader reader, out string chunkId, out uint chunkSize)
+        {
+            chunkId = null;
+            chunkSize = 0;
+
+            if (reader.BaseStream.Length - reader.BaseStream.Position < 8)
+            {
+                return false;
+            }
+
+            chunkId = ReadFourCc(reader);
+            chunkSize = reader.ReadUInt32();
+            return true;
+        }
+
+        private static long GetReadableChunkSize(uint declaredSize, long bytesRemaining)
+        {
+            if (bytesRemaining < 0)
+            {
+                return -1;
+            }
+
+            if (declaredSize == uint.MaxValue)
+            {
+                return bytesRemaining;
+            }
+
+            return Math.Min((long)declaredSize, bytesRemaining);
         }
 
         private static AudioFrame CreateFallbackFrame(float volumeFactor)
